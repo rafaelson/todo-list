026a437bdc5bbc3606e419771c562355348d3d29
Rafael Nelson <rafaelson1337@gmail.com>
2022-06-08 15:41:18 -0300

    rough layout, to be totally refactored

--- src/App.js
+++ src/App.js
@@ -1,23 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Drawer,
+  Divider,
+  Typography,
+  List,
+  ListItem,
+  ListItemButton,
+  AppBar,
+  Toolbar,
+  ListItemText,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  IconButton,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { Add, Delete } from "@mui/icons-material";
+import { Fragment } from "react";
+
+const drawerWidth = "240px";
+const card = (
+  <Fragment>
+    <CardContent>
+      <FormGroup>
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+        <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+      </FormGroup>
+      {/* <Typography>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti,
+        tempora. Vel, aliquam. Distinctio saepe quos maiores vel hic cum rerum
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti,
+        tempora. Vel, aliquam. Distinctio saepe quos maiores vel hic cum rerum
+        nemo illum molestias sed, amet iure fugit reiciendis deleniti. Quaerat.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti,
+        tempora. Vel, aliquam. Distinctio saepe quos maiores vel hic cum rerum
+        nemo illum molestias sed, amet iure fugit reiciendis deleniti. Quaerat.
+      </Typography> */}
+    </CardContent>
+    <CardActions sx={{ justifyContent: "flex-end" }}>
+      <IconButton>
+        <Add />
+      </IconButton>
+    </CardActions>
+  </Fragment>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box sx={{ display: "flex", flexDirection: "row" }}>
+        <AppBar sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+          <Toolbar variant="dense" sx={{ justifyContent: "space-between" }}>
+            <Typography variant="h5">ToDo App</Typography>
+            <IconButton>
+              <Add />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Toolbar />
+          <Box sx={{ overflow: "auto" }}>
+            <List>
+              <ListItem>
+                <ListItemButton>
+                  <ListItemText primary="Default project" />
+                </ListItemButton>
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <ListItemButton>
+                  <ListItemText primary="Default project" />
+                </ListItemButton>
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <ListItemButton>
+                  <ListItemText primary="Default project" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem sx={{ justifyContent: "flex-end" }}>
+                <IconButton>
+                  <Add />
+                </IconButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          <Box
+            sx={{
+              display: "grid",
+              gap: 5,
+              gridTemplateColumns: "repeat(auto-fit, minmax(150px, 250px))",
+            }}
+          >
+            <Card
+              variant="outlined"
+              sx={{
+                minWidth: 230,
+                maxWidth: 280,
+                height: 300,
+                overflow: "auto",
+              }}
+            >
+              {card}
+            </Card>
+            <Card
+              variant="outlined"
+              sx={{
+                minWidth: 230,
+                maxWidth: 280,
+                height: 300,
+                overflow: "auto",
+              }}
+            >
+              {card}
+            </Card>
+          </Box>
+        </Box>
+      </Box>
     </div>
   );
 }
